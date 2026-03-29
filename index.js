743f2b3d1445c8d27bbf19e4d765bbcbe8949a54
jawadaadilwebdev <jawadaadil.webdev@gmail.com>
2026-03-29 03:45:44 -0700

    Add express dependency and start script to package.json

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
-console.log("This is a simple Node.js application.");
+// console.log("Hello World");
+// console.log("This is a simple Node.js application.");
+let express = require('express');
+let app = express();
+
+app.get("/",(req,res)=>{
+    res.send({status : 1 , msg : "Home Page"})
+})
+app.get("/news",(req,res)=>{
+    res.send({status : 2 , msg : "News Page"})
+})
+
+app.listen("8000")
