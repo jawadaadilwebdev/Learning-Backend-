29ace7772e9dabe02976e0cfaa5f36a2a724742d
jawadaadilwebdev <jawadaadil.webdev@gmail.com>
2026-03-29 03:56:43 -0700

    Add POST /login endpoint to handle user login requests

--- index.js
+++ index.js
@@ -2,12 +2,18 @@
 // console.log("This is a simple Node.js application.");
 let express = require('express');
 let app = express();
+app.use(express.json())
 
 app.get("/",(req,res)=>{
     res.send({status : 1 , msg : "Home Page"})
 })
 app.get("/news",(req,res)=>{
     res.send({status : 2 , msg : "News Page"})
+})
+
+app.post("/login",(req,res)=>{
+    console.log(req.body);
+    res.send({status : 3 , msg : "Login Page",data : req.body})
 })
 
 app.listen("8000")
